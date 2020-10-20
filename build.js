800aaa9f05c7497c11eb981d1c4655feb0e93ca4
zadam <adam.zivner@gmail.com>
2020-10-20 23:09:40 +0200

    release 0.2.0

--- build.js
+++ build.js
@@ -1,1 +1,1 @@
-module.exports = { buildDate:"2020-10-20T23:09:01+02:00", buildRevision: "01525464a40a60946eddc5d4f6090017d4e7b479" };
+module.exports = { buildDate:"2020-10-20T23:09:40+02:00", buildRevision: "2a79ef0aa5cfbe56ed0d2bd2739760ef495aa04a" };
