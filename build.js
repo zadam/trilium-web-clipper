91309802034f655664fbe0973dab5d239313396a
zadam <zadam.apps@gmail.com>
2022-10-29 14:59:05 +0200

    release 1.0.0

--- build.js
+++ build.js
@@ -1,1 +1,1 @@
-module.exports = { buildDate:"2022-10-29T14:56:00+02:00", buildRevision: "5007ec470331b969c61d32a422772447a0aa6f18" };
+module.exports = { buildDate:"2022-10-29T14:59:05+02:00", buildRevision: "efb24366797442ff8d434db0b68ec6a9f5338dd1" };
